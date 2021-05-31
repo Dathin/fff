b6221f8de6cbd944234fcc929e74e8ed58e4ec24
Pedro Caires <pedro.caires@iteris.com.br>
2021-05-31 20:43:30 -0300

    test: swagger and postgres configs

---
+++ src/test/java/me/pedrocaires/fff/database/PostgresConfigTest.java
@@ -0,0 +1,32 @@
+package me.pedrocaires.fff.database;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class PostgresConfigTest {
+
+    @Mock
+    DataSource dataSource;
+
+    @InjectMocks
+    PostgresConfig postgresConfig;
+
+    @Test
+    void shouldReturnDataSource() {
+        assertNotNull(postgresConfig.postgresDataSource());
+    }
+
+    @Test
+    void shouldReturnJdbcTemplate() {
+        assertNotNull(postgresConfig.postgresJdbcTemplate(dataSource));
+    }
+}
